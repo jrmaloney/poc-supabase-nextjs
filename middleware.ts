d11ce56eac5ce1fca068ca64250c0d1dced24903
John <jrmaloney@gmail.com>
2025-03-14 15:34:14 -0400

    site offline via middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
+import { NextResponse } from "next/server";
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
   // update user's auth session
-  return await updateSession(request)
+  //return await updateSession(request)
+  return new NextResponse("Site is offline", { status: 403 });
+
+
 }
 
 export const config = {
